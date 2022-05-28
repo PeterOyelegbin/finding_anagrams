f13aaadf2b2893a35d567e302a8902c504a7ea85
Peter Oyelegbin <peteroyelegbin@gmail.com>
2022-05-28 13:04:09 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# Check if a word is an anagrams 
+# Example:
+# find_anagrams("hello") --> False
+# find_anagrams("racecar") --> True
+
+def find_anagrams(word1, word2):
+    # [assignment] Add your code here
+    w1 = sorted(word1.lower())
+    w2 = sorted(word2.lower())
+    
+    if (w1 == w2):
+        print("True")
+    else:
+        print("False")
+        
+    return True
+
+
+find_anagrams("Heart", "Earth")
